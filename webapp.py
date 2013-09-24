86e8cf6358e805d24f640f4405f4348cff20e50b
Edouard <edou4rd@gmail.com>
2013-09-24 18:33:41 +0200

    babel removed

--- webapp.py
+++ webapp.py
@@ -11,22 +11,14 @@
 # Last mod : 24-Sep-2013
 # -----------------------------------------------------------------------------
 
-from flask import Flask, render_template, request
-from flask.ext.babel import Babel
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
-babel = Babel(app)
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
-
-@babel.localeselector
-def get_locale():
-    # try to guess the language from the user accept
-    return None
-    return request.accept_languages.best_match(['de', 'fr', 'en'])
 
 if __name__ == "__main__":
     app.debug = True
